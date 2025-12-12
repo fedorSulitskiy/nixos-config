05d54ef8df4962369a81818f1adbac60aa17413f
Fedor Sulitskiy <72353528+fedorSulitskiy@users.noreply.github.com>
2025-12-12 15:28:57 +0000

    Bug: Fixed postgres not authenticating my requests from ::1

--- modules/devTools/postgres.nix
+++ modules/devTools/postgres.nix
@@ -21,8 +21,12 @@
       authentication = pkgs.lib.mkOverride 10 ''
         # TYPE  DATABASE        USER            ADDRESS                 METHOD
         local   all             all                                     trust
+        host    all             all             ::1/128                 trust
         host    all             all             127.0.0.1/32            trust
       '';
+      settings = {
+        listen_addresses = "localhost";
+      };
       extensions = with pkgs.postgresql_16.pkgs; [pgvector];
       package = pkgs.postgresql_16;
     };
