57dff56a2cd88483f81609281f6fd0af3f544667
fedorSulitskiy <f.sulitskiy@gmail.com>
2026-02-28 19:06:26 +0000

    Feat/Desktop: Updated to nixpkgs unstable

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Fedor's Systems Flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     zen-browser = {
